d6eb3818b6344f3caf2bfa9e1c97a8f36b78a72c
Don Morgan <don.morgan@gov.bc.ca>
2018-02-26 10:32:13 -0800

    create header.R

---
+++ header.R
@@ -0,0 +1,15 @@
+library(sf)
+library(dplyr)
+library(readr)
+
+OutDir <- 'out'
+dataOutDir <- file.path(OutDir,'data')
+tileOutDir <- file.path(dataOutDir,'tile')
+figsOutDir <- file.path(OutDir,'figures/')
+DataDir <- 'data'
+dir.create(file.path(OutDir), showWarnings = FALSE)
+dir.create(file.path(dataOutDir), showWarnings = FALSE)
+dir.create(file.path(tileOutDir), showWarnings = FALSE)
+dir.create(file.path(figsOutDir), showWarnings = FALSE)
+dir.create(DataDir, showWarnings = FALSE)
+
