553ab8930e16fbfbd9ef84249370fd04433f1d1b
Don Morgan <don.morgan@gov.bc.ca>
2018-03-05 14:20:50 -0800

    Load in Lakes from BTM and rasterize

--- 01_load.R
+++ 01_load.R
@@ -16,9 +16,11 @@ RoadDens<-raster(file.path(DataDir,"RoadDensR.tif"))
 
 #Rasterize the Province for subsequent masking
 ProvRast<-raster(nrows=15744, ncols=17216, xmn=159587.5, xmx=1881187.5, ymn=173787.5,ymx=1748187.5,crs="+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0",res=c(100,100),vals=0)
+
 BCr_file <- file.path(dataOutDir,"BCr.tif")
 if (!file.exists(BCr_file)) {
   BCr <- rasterize(bcmaps::bc_bound_hres(),ProvRast,mask=TRUE, filename = BCr_file)
+  writeRaster(BCr, filename=file.path(dataOutDir,"BCr.tif"), format="GTiff", overwrite=TRUE)
 } else {
   BCr <- raster(BCr_file)
 }
@@ -27,5 +29,32 @@ if (!file.exists(BCr_file)) {
 RoadDensP100<-crop(RoadDens,ProvRast)
 
 #write out processed rasters
-#writeRaster(BCr, filename=file.path(dataOutDir,"BCr.tif"), format="GTiff", overwrite=TRUE)
 #writeRaster(RoadDensP100, filename=file.path(dataOutDir,"RoadDensP100.tif"), format="GTiff", overwrite=TRUE)
+
+# Read in or create Lakes raster
+LakesR_file <- file.path(dataOutDir,"LakesR.tif")
+
+if (!file.exists(LakesR_file)) {
+  # Link to BTM file download from BCDC:
+  # https://catalogue.data.gov.bc.ca/dataset/baseline-thematic-mapping-present-land-use-version-1-spatial-layer
+  #Dowload file manually and put *.zip in this script and place file in the data directory
+  BTMZip <- 'BCGW_78757263_1520272242999_7572.zip'
+  unzip(file.path(DataDir, BTMZip), exdir = file.path(DataDir, "Lakes"))
+  
+  # List feature classes in the geodatabase
+  BTM_gdb <- list.files(file.path(DataDir, "Lakes"), pattern = ".gdb", full.names = TRUE)[1]
+  fc_list <- st_layers(BTM_gdb)
+  
+  # Read as sf and flip to spatial to rasterize
+  BTM <- as(read_sf(BTM_gdb, layer = "WHSE_BASEMAPPING_BTM_PRESENT_LAND_USE_V1_SVW"),'Spatial')
+  # Pull out lakes
+  Lakes<-subset(BTM,BTM@data$PRESENT_LAND_USE_LABEL=='Fresh Water')
+  # Rasterize lakes and set to a binary 
+  LakesR<-(rasterize(Lakes,ProvRast))>0
+  # Write out raster
+  writeRaster(LakesR, filename=file.path(dataOutDir,"LakesR.tif"), format="GTiff", overwrite=TRUE)
+  } else {
+  LakesR <- raster(LakesR_file)
+}
+
+
