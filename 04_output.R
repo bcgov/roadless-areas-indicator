8d3376d8f1d044d2b298a9483f6d9aea2c23f748
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-03-22 16:14:07 -0700

    reorg code chunks

--- 04_output.R
+++ 04_output.R
@@ -81,34 +81,6 @@ ecoreg_summary <- map_df(rbyp_par_summary, ~ {
 #clean up the workspace
 gc()
 
-#summary of results
-
-bc_area_summary <- ecoreg_summary %>% 
-  filter(name == "Province") %>% 
-  group_by(name, roaded_class) %>% 
-  mutate(total_area=sum(area_ha),
-         total_perc=sum(percent_in_distance_class)) %>% 
-  filter(distance_class != ">5000") %>% 
-  mutate(distance_class = recode(distance_class, "500-5000" = ">500")) %>% 
-  ungroup() %>% 
-  dplyr::select(-area_ha, -percent_in_distance_class) 
-bc_area_summary
-
-
-ecoregion_area_summary <- ecoreg_summary %>% 
-  filter(name != "Province") %>% 
-  group_by(name, roaded_class) %>% 
-  mutate(total_area=sum(area_ha),
-         total_perc=sum(percent_in_distance_class)) %>% 
-  filter(distance_class != ">5000") %>% 
-  mutate(distance_class = recode(distance_class, "500-5000" = ">500")) %>% 
-  ungroup() %>% 
-  dplyr::select(-area_ha, -percent_in_distance_class) %>% 
-  filter(roaded_class == "Not Roaded")
-ecoregion_area_summary
-
-more50 <- ecoregion_area_summary %>% count(total_perc > 50)
-less25 <- ecoregion_area_summary %>% count(total_perc < 25)
 
 #### FUNCTIONS
 #A set of functions that will be called for displaying table, map and graphs
@@ -258,3 +230,32 @@ for (n in names(plot_list)) {
   plot(map)
   dev.off()
 }
+
+#summary of results
+
+bc_area_summary <- ecoreg_summary %>% 
+  filter(name == "Province") %>% 
+  group_by(name, roaded_class) %>% 
+  mutate(total_area=sum(area_ha),
+         total_perc=sum(percent_in_distance_class)) %>% 
+  filter(distance_class != ">5000") %>% 
+  mutate(distance_class = recode(distance_class, "500-5000" = ">500")) %>% 
+  ungroup() %>% 
+  dplyr::select(-area_ha, -percent_in_distance_class) 
+bc_area_summary
+
+
+ecoregion_area_summary <- ecoreg_summary %>% 
+  filter(name != "Province") %>% 
+  group_by(name, roaded_class) %>% 
+  mutate(total_area=sum(area_ha),
+         total_perc=sum(percent_in_distance_class)) %>% 
+  filter(distance_class != ">5000") %>% 
+  mutate(distance_class = recode(distance_class, "500-5000" = ">500")) %>% 
+  ungroup() %>% 
+  dplyr::select(-area_ha, -percent_in_distance_class) %>% 
+  filter(roaded_class == "Not Roaded")
+ecoregion_area_summary
+
+more50 <- ecoregion_area_summary %>% count(total_perc > 50)
+less25 <- ecoregion_area_summary %>% count(total_perc < 25)
